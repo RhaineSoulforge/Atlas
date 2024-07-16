df9be6a39eaa7550e1ae64f33a8b5dc3bd0c908d
Jason Alexander Biddle <biddle.jason.a@gmail.com>
2024-07-15 21:58:32 -0400

    Daily Update (07.15.24)
    Working Maths Library completed!

--- Atlas/src/Atlas/Utils/Maths/Matrix.h
+++ Atlas/src/Atlas/Utils/Maths/Matrix.h
@@ -278,6 +278,103 @@ namespace Math
             return result;
         }
 
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Function:  RotateX
+        // Last Modified:  July 15, 2024
+        // Author:  Jason A Biddle
+        //
+        // Purpose:  Rotates about the X-axis by a given angle.
+        //
+        // In:  fAngle - The angle in degrees to be rotated by.
+        //
+        // Out:  --
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        void RotateX(float fAngle)
+        {
+            float fRads = DegreestoRadians(fAngle);
+            Matrix rotation;
+
+            rotation.m_up._Y = std::cos(fRads);
+            rotation.m_up._Z = std::sin(fRads);
+
+            rotation.m_forward._Y = -std::sin(fRads);
+            rotation.m_forward._Z = std::cos(fRads);
+
+            *this = this->Multiply(rotation);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Function: RotateY
+        // Last Modified:  July 15, 2024
+        // Author:  Jason A Biddle
+        //
+        // Purpose:  Rotates about the Y-axis by a given angle.
+        //
+        // In:  fAngle - The angle in degrees to be rotate by.
+        //
+        // Out:  --
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        void RotateY(float fAngle)
+        {
+            float fRads = DegreestoRadians(fAngle);
+            Matrix rotation;
+
+            rotation.m_right._X = std::cos(fRads);
+            rotation.m_right._Z = -std::sin(fRads);
+
+            rotation.m_forward._X = std::sin(fRads);
+            rotation.m_forward._Z = std::cos(fRads);
+
+            *this = this->Multiply(rotation);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Function: RotateZ
+        // Last Modified:  July 15, 2024
+        // Author:  Jason A Biddle
+        //
+        // Purpose:  Rotates about the Z-axis by a given angle.
+        //
+        // In:  fAngle - The angle in degrees to be rotate by.
+        //
+        // Out:  --
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        void RotateZ(float fAngle)
+        {
+            float fRads = DegreestoRadians(fAngle);
+            Matrix rotation;
+
+            rotation.m_right._X = std::cos(fRads);
+            rotation.m_right._Y = std::sin(fRads);
+
+            rotation.m_up._X = -std::sin(fRads);
+            rotation.m_up._Y = std::cos(fRads);
+
+            *this = this->Multiply(rotation);
+        }
+
+        void Scale(float fX, float fY, float fZ)
+        {
+            Matrix scalar;
+            scalar.m_right._X = fX;
+            scalar.m_up._Y = fY;
+            scalar.m_forward._Z = fZ;
+
+            *this = this->Multiply(scalar);
+        }
+
+        void Translate(float fX, float fY, float fZ)
+        {
+            m_position._X += fX;
+            m_position._Y += fY;
+            m_position._Z += fZ;
+        }
+
+        void Translate(const Vector4f &distance)
+        {
+            m_position += distance;
+        }
+
         Matrix &operator=(const Matrix &rhs)
         {
             m_right = rhs.m_right;
@@ -287,196 +384,4 @@ namespace Math
             return *this;
         }
     };
-    // struct Matrix3d
-    // {
-    //     Vector3f m_right;
-    //     Vector3f m_up;
-    //     Vector3f m_position;
-
-    //     Matrix3d()
-    //     {
-    //         m_right.m_arfDirection[0] = 1.0f;
-    //         m_up.m_arfDirection[1] = 1.0f;
-    //         m_position.m_arfDirection[2] = 1.0f;
-    //     }
-
-    //     Matrix3d(const Matrix3d &rhs)
-    //     {
-    //         m_right = rhs.m_right;
-    //         m_up = rhs.m_up;
-    //         m_position = rhs.m_position;
-    //     }
-
-    //     Matrix3d(const Vector3f &right, const Vector3f &up, const Vector3f &position)
-    //     {
-    //         m_right = right;
-    //         m_up = up;
-    //         m_position = position;
-    //     }
-
-    //     void to_array(float *out) const
-    //     {
-    //         out[0] = m_right.m_arfDirection[0];
-    //         out[3] = m_right.m_arfDirection[1];
-    //         out[6] = m_right.m_arfDirection[2];
-
-    //         out[1] = m_up.m_arfDirection[0];
-    //         out[4] = m_up.m_arfDirection[1];
-    //         out[7] = m_up.m_arfDirection[2];
-
-    //         out[2] = m_position.m_arfDirection[0];
-    //         out[5] = m_position.m_arfDirection[1];
-    //         out[8] = m_position.m_arfDirection[2];
-    //     }
-
-
-    //     std::string to_string(void)
-    //     {
-    //         std::stringstream buffer;
-    //         buffer << std::fixed << std::setprecision(4) << std::setfill(' ');
-    //         buffer << '|' << m_right.m_arfDirection[0] << " " << m_up.m_arfDirection[0] << " " << m_position.m_arfDirection[0] << "|\n";
-    //         buffer << '|' << m_right.m_arfDirection[1] << " " << m_up.m_arfDirection[1] << " " << m_position.m_arfDirection[1] << "|\n";
-    //         buffer << '|' << m_right.m_arfDirection[2] << " " << m_up.m_arfDirection[2] << " " << m_position.m_arfDirection[2] << "|\n";
-    //         return buffer.str();
-    //     }
-
-    //     void SetIdentity(void)
-    //     {
-    //         //Todo after writting operator=
-    //         m_right.m_arfDirection[0] = m_up.m_arfDirection[1] = m_position.m_arfDirection[2] = 1.0f;
-
-    //         m_right.m_arfDirection[1] = m_right.m_arfDirection[2] = 0.0f;
-    //         m_up.m_arfDirection[0] = m_up.m_arfDirection[2] = 0.0f;
-    //         m_position.m_arfDirection[0] = m_position.m_arfDirection[1] = 0.0f;
-    //     }
-
-    //     float Det(void)
-    //     {
-    //         float result;
-
-    //         result = m_right.m_arfDirection[0] * ((m_up.m_arfDirection[1]*m_position.m_arfDirection[2]) - (m_position.m_arfDirection[1]*m_up.m_arfDirection[2])) -
-    //                  m_up.m_arfDirection[0] * ((m_right.m_arfDirection[1]*m_position.m_arfDirection[2]) - (m_position.m_arfDirection[1]*m_right.m_arfDirection[2])) +
-    //                  m_position.m_arfDirection[0] * ((m_right.m_arfDirection[1]*m_up.m_arfDirection[2]) - (m_right.m_arfDirection[2]*m_up.m_arfDirection[1]));
-
-    //         return result;
-    //     }
-
-    //     void Inverse(void)
-    //     {
-    //         float lhs[9];
-    //         to_array(lhs);
-    //         float det = 1.0f / Det();
-    //         std::cout << "Det(f) := " << Det() << std::endl;
-
-    //         //Matrix of Minors
-    //         float minors[9];
-    //         minors[0] = (lhs[4] * lhs[8]) - (lhs[5] * lhs[7]);
-    //         minors[1] = (lhs[3] * lhs[8]) - (lhs[5] * lhs[6]);
-    //         minors[2] = (lhs[3] * lhs[7]) - (lhs[4] * lhs[6]);
-
-    //         minors[3] = (lhs[1] * lhs[8]) - (lhs[2] * lhs[7]);
-    //         minors[4] = (lhs[0] * lhs[8]) - (lhs[2] * lhs[6]);
-    //         minors[5] = (lhs[0] * lhs[7]) - (lhs[1] * lhs[6]);
-
-    //         minors[6] = (lhs[1] * lhs[5]) - (lhs[2] * lhs[4]);
-    //         minors[7] = (lhs[0] * lhs[5]) - (lhs[2] * lhs[3]);
-    //         minors[8] = (lhs[0] * lhs[4]) - (lhs[1] * lhs[3]);
-
-    //         std::cout << std::fixed << std::setprecision(20);
-    //         for(int row=0;row<3;row++)
-    //         {
-    //             std::cout << "|";
-    //             for(int col=0;col<3;col++)
-    //             {
-    //                 if(col == 2)
-    //                     std::cout << minors[col + (row*3)] << "|";
-    //                 else
-    //                     std::cout << minors[col + (row*3)] << " ";
-    //             }
-    //             std::cout << std::endl;
-    //         }
-    //         m_right.m_arfDirection[0] = minors[0] * det;
-    //         m_right.m_arfDirection[1] = minors[1] * det * -1.0f;
-    //         m_right.m_arfDirection[2] = minors[2] * det;
-
-    //         m_up.m_arfDirection[0] = minors[3] * det * -1.0f;
-    //         m_up.m_arfDirection[1] = minors[4] * det;
-    //         m_up.m_arfDirection[2] = minors[5] * det * -1.0f;
-
-    //         m_position.m_arfDirection[0] = minors[6] * det;
-    //         m_position.m_arfDirection[1] = minors[7] * det * -1.0f;
-    //         m_position.m_arfDirection[2] = minors[8] * det;
-    //     }
-
-    //     void RotateX(float fAngle)
-    //     {
-    //         Matrix3d rotmatrix;
-
-    //         m_up.m_arfDirection[1] = std::cos(fAngle);
-    //         m_up.m_ar
-    //     }
-
-    //     void RotateY(float fAngle)
-    //     {
-    //     }
-
-    //     void RotateZ(float fAngle)
-    //     {
-    //     }
-
-    //     void Scale(float scalarX, float scalarY, float scalarZ)
-    //     {
-    //         Matrix3d ID;
-    //         ID.m_right.m_arfDirection[0] = scalarX;
-    //         ID.m_up.m_arfDirection[1] = scalarY;
-    //         ID.m_position.m_arfDirection[2] = scalarZ;
-    //         Multiply(ID);
-    //     }
-
-    //     void Translate(float x, float y)
-    //     {
-    //         Math::Vector3f translate(x,y,0.0f);
-    //         m_position += translate;
-    //     }
-
-    //     void Translate(const Vector3f &distance)
-    //     {
-    //         m_position += distance;
-    //     }
-
-    //     void Multiply(const Matrix3d &rhs)
-    //     {
-    //         float lhs[9];
-    //         float rhm[9];
-
-    //         to_array(lhs);
-    //         rhs.to_array(rhm);
-
-    //         m_right.m_arfDirection[0] = (lhs[0]*rhm[0]) + (lhs[1]*rhm[3]) + (lhs[2]*rhm[6]);
-    //         m_up.m_arfDirection[0] = (lhs[0]*rhm[1]) + (lhs[1]*rhm[4]) + (lhs[2]*rhm[7]);
-    //         m_position.m_arfDirection[0] = (lhs[0]*rhm[2]) + (lhs[1]*rhm[5]) + (lhs[2]*rhm[8]);
-
-    //         m_right.m_arfDirection[1] = (lhs[3]*rhm[0]) + (lhs[4]*rhm[3]) + (lhs[5]*rhm[6]);
-    //         m_up.m_arfDirection[1] = (lhs[3]*rhm[1]) + (lhs[4]*rhm[4]) + (lhs[5]*rhm[7]);
-    //         m_position.m_arfDirection[1] = (lhs[3]*rhm[2]) + (lhs[4]*rhm[5]) + (lhs[5]*rhm[8]);
-
-    //         m_right.m_arfDirection[2] = (lhs[6]*rhm[0]) + (lhs[7]*rhm[3]) + (lhs[8]*rhm[6]);
-    //         m_up.m_arfDirection[2] = (lhs[6]*rhm[1]) + (lhs[7]*rhm[4]) + (lhs[8]*rhm[7]);
-    //         m_position.m_arfDirection[2] = (lhs[6]*rhm[2]) + (lhs[7]*rhm[5]) + (lhs[8]*rhm[8]);
-    //     }
-
-    //     Matrix3d &operator*(const Matrix3d &rhs)
-    //     {
-    //         this->Multiply(rhs);
-    //         return *this;
-    //     }
-
-    //     Matrix3d &operator=(const Matrix3d &rhs)
-    //     {
-    //         m_right = rhs.m_right;
-    //         m_up = rhs.m_up;
-    //         m_position = rhs.m_position;
-    //         return *this;
-    //     }
-    // };
 }
