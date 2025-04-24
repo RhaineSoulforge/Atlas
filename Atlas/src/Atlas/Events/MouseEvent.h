704c06049caf470b8ac165e74add60dedc597ca0
RhaineSoulforge <biddle.jason.a@gmail.com>
2025-04-24 07:22:10 -0400

    Finished implementing events.

--- Atlas/src/Atlas/Events/MouseEvent.h
+++ Atlas/src/Atlas/Events/MouseEvent.h
@@ -5,5 +5,108 @@
 
 namespace Atlas
 {
-    
+    class CMouseMovedEvent : public CEvent
+    {
+        public:
+            CMouseMovedEvent(const float x, const float y)
+                : m_fMouseX(x), m_fMouseY(y)
+            {
+
+            }
+
+            float GetX(void) const { return m_fMouseX; }
+            float GetY(void) const { return m_fMouseY; }
+
+            std::string ToString(void) const override
+            {
+                std::stringstream ss;
+                ss << "MouseMovedEvent: " << m_fMouseX << ", " << m_fMouseY;
+                return ss.str();
+            }
+
+            EVENT_CLASS_TYPE(eEventType::MouseMoved)
+            EVENT_CLASS_CATEGORY(eEventCategory::EventCategoryMouse | eEventCategory::EventCategoryInput)
+
+        private:
+            float m_fMouseX;
+            float m_fMouseY;
+    };
+
+    class CMouseScrolledEvent : public CEvent
+    {
+        public:
+            CMouseScrolledEvent(const float xOffset, const float yOffset)
+                : m_fXOffset(xOffset), m_fYOffset(yOffset)
+            {
+
+            }
+
+            float GetXOffset(void) const { return m_fXOffset; }
+            float GetYOffset(void) const { return m_fYOffset; }
+
+            std::string ToString(void) const override
+            {
+                std::stringstream ss;
+                ss << "MouseScrolledEvent: " << m_fXOffset << ", " << m_fYOffset;
+                return ss.str();
+            }
+
+            EVENT_CLASS_TYPE(eEventType::MouseScrolled)
+            EVENT_CLASS_CATEGORY(eEventCategory::EventCategoryMouse | eEventCategory::EventCategoryInput)
+
+        private:
+            float m_XOffset;
+            float m_YOffset;
+    };
+
+    class CMouseButtonEvent : public CEvent
+    {
+        public:
+            int GetMouseButton(void) const { return m_nButton; }
+
+            EVENT_CLASS_CATEGORY(eEventCategory::EventCategoryMouseButton | eEventCategory::EventCategoryInput | eEventCategory::EventCategoryMouseButton)
+        protected:
+            CMouseButtonEvent(const int button) : m_nButton(button)
+            {
+
+            }
+
+            int m_nButton;
+    };
+
+    class CMouseButtonPressedEvent : public CMouseButtonEvent
+    {
+        public:
+            CMouseButtonPressedEvent(const int button) : m_nButton(button)
+            {
+
+            }
+
+            std::string ToString(void) const override
+            {
+                std::stringstream ss;
+                ss << "MouseButtonPressedEvent: " << m_nButton;
+                return ss.str();
+            }
+
+            EVENT_CLASS_TYPE(eEventType::MouseButtonPressed)
+    };
+
+    class CMouseButtonReleasedEvent : public CMouseButtonEvent
+    {
+        public:
+            CMouseButtonReleasedEvent(const int button) : m_nButton(button)
+            {
+
+            }
+
+            std::string ToString(void) const override
+            {
+                std::stringstream ss;
+                ss << "MouseButtonReleasedEvent: " << m_nButton;
+                return ss.str();
+            }
+
+            EVENT_CLASS_TYPE(eEventType::MouseButtonReleased)
+    };
 }
